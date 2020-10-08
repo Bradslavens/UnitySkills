2f388ee8899c04194e0e74d5441edcf3ddc09008
Brad Slavens <Bradslavens@users.noreply.github.com>
2020-10-08 11:01:39 -0700

    added c# script for drag and drop

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool isDraggable = true;
+    public bool isDragged = false;
+    public Transform transform;
+
 
     // Update is called once per frame
     void Update()
     {
+        if(isDragged){
+
+            transform.position = Input.mousePosition;
+        }
+    }
+
+    private void OnMouseOver() {
+        if(isDraggable && Input.GetMouseButtonDown(0)){
+            isDragged = true;
+        }
         
     }
+
+    private void OnMouseUp() {
+        isDragged = false;
+    } 
+
 }
