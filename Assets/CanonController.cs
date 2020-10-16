14ac58a810ee50eb3c92cc10f0a7a3ad529121fe
Brad Slavens <Bradslavens@users.noreply.github.com>
2020-10-16 09:18:16 -0700

    added rotation to canon

---
+++ Assets/CanonController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonController : MonoBehaviour
+{
+    public Rigidbody2D canon;
+    public float rotationSpeed = 0.5f;
+
+    private void Start() {
+        canon.gravityScale = 0;
+    }
+
+    private void Update() {
+        var x = Input.GetAxis("Vertical") * Time.deltaTime * rotationSpeed;
+        transform.Rotate(0,0,x);
+    }
+
+    private void FixedUpdate() {
+        // canon.MoveRotation(canon.rotation + rotationSpeed + Time.fixedDeltaTime);
+    }
+}
