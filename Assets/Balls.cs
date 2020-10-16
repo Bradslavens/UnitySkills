ecef5305ce44ba3fdbd0c3b9e3eaf7b12f5f171e
Brad Slavens <Bradslavens@users.noreply.github.com>
2020-10-16 09:37:06 -0700

    added cannonballs

---
+++ Assets/Balls.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balls : MonoBehaviour
+{
+    public Rigidbody2D cannonBall;
+    public Transform barrelPosition;
+
+    private void Update() {
+        if(Input.GetMouseButtonDown(0)){
+            Rigidbody2D cannonBallInstance;
+            cannonBallInstance = Instantiate(cannonBall, barrelPosition.position, barrelPosition.rotation) as Rigidbody2D;
+            cannonBallInstance.AddForce(barrelPosition.up *1000f);
+        }
+    }
+}
--- Assets/Destruction.cs
+++ Assets/Destruction.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Destruction : MonoBehaviour
 {
+    public float destroyIn = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 0.2f);
+        Destroy(gameObject, destroyIn);
     }
 
 }
