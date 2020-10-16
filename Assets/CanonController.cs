fcae5416c11bf362d34e71f9754412266f2edd8c
Brad Slavens <Bradslavens@users.noreply.github.com>
2020-10-16 10:40:07 -0700

    added cinemachine camera to cannon for test

--- Assets/CanonController.cs
+++ Assets/CanonController.cs
@@ -7,16 +7,24 @@ public class CanonController : MonoBehaviour
     public Rigidbody2D canon;
     public float rotationSpeed = 0.5f;
 
+    private Vector2 movPos;
+
+
+
     private void Start() {
         canon.gravityScale = 0;
     }
 
     private void Update() {
-        var x = Input.GetAxis("Vertical") * Time.deltaTime * rotationSpeed;
-        transform.Rotate(0,0,x);
+        // var x = Input.GetAxis("Vertical") * Time.deltaTime * rotationSpeed;
+        // transform.Rotate(0,0,x);
+
+        movPos.x = Input.GetAxis("Horizontal");
+        movPos.y = Input.GetAxis("Vertical");
     }
 
     private void FixedUpdate() {
         // canon.MoveRotation(canon.rotation + rotationSpeed + Time.fixedDeltaTime);
+        canon.MovePosition(canon.position + movPos * rotationSpeed * Time.fixedDeltaTime);
     }
 }
